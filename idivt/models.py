acce88f473d1556f777d42f3a786b99c51a84099
gerard <gerar.f87@gmail.com>
2017-01-31 16:17:43 +0100

    Added models

--- idivt/models.py
+++ idivt/models.py
@@ -3,3 +3,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Line(models.Model):
+
+    name = models.TextField()
+
+    def __unicode__(self):
+        return str(self.name)
+
+class Tower(models.Model):
+
+    name = models.TextField()
+    line = models.ForeignKey(Line)
+    longitude = models.DecimalField(max_digits=20, decimal_places=15)
+    latitude = models.DecimalField(max_digits=20, decimal_places=15)
+    altitude = models.DecimalField(max_digits=20, decimal_places=15)
+
+    def __unicode__(self):
+        return str(self.name)
